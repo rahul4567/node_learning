e53dd0b82c51948efb0788197a09ae1dae8424d3
rahul4567 <rahul.edu.in@gmail.com>
2025-02-10 04:46:59 +0530

    Express multiple routes

--- app.js
+++ app.js
@@ -9,6 +9,114 @@ const app = express();
 //   res.send("Namshte Rahul");
 // });
 
+//multple route handler
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log(req.query);
+    console.log(req.params);
+    // res.send({
+    //   firstName: "Rahul",
+    //   lastName: "Kumar",
+    // });
+    console.log(
+      "Will it got to next route handle, no it wont as unless next is used"
+    );
+    next();
+  },
+  (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
+    res.send({
+      firstName: "Rahul_2nd handlre",
+      lastName: "Kumar",
+    });
+  }
+);
+app.get(
+  "/test2",
+  (req, res, next) => {
+    console.log(req.query);
+    console.log(req.params);
+    console.log("It will send first route response give error");
+    res.send({
+      firstName: "Rahul",
+      lastName: "Kumar",
+    });
+
+    next();
+  },
+  (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
+    res.send({
+      firstName: "Rahul_2nd handlre",
+      lastName: "Kumar",
+    });
+  }
+);
+//Multiple route handler, we can write as many routes
+app.get(
+  "/test3",
+  (req, res, next) => {
+    console.log("First Route");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd handler");
+    next();
+  },
+  (req, res) => {
+    res.send({
+      firstName: "Rahul_3rd handlre",
+      lastName: "Kumar",
+    });
+  }
+);
+
+//multiple handler with array syntax
+
+app.get("/test4", [
+  (req, res, next) => {
+    console.log("It will send first route response give error");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd handler");
+    next();
+  },
+  (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
+    res.send({
+      firstName: "Rahul_3rd handlre",
+      lastName: "Kumar",
+    });
+  },
+]);
+app.get(
+  "/test6",
+  [
+    (req, res, next) => {
+      console.log("first route handler");
+      next();
+    },
+    (req, res, next) => {
+      console.log("2nd handler");
+      next();
+    },
+  ],
+  [
+    (req, res) => {
+      console.log(req.query);
+      console.log(req.params);
+      res.send({
+        firstName: "Rahul_3rd handlre",
+        lastName: "Kumar",
+      });
+    },
+  ]
+);
 //advance routing
 //work for ac abc
 app.get("/ab?c", (req, res) => {
